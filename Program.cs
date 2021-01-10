dcad3068ae87fa942dee1a522a478a742b069728
Katherine Trammell <ktrammell.com>
2021-01-10 01:31:22 -0500

    Making a mess and trying to figure out Classes and Methods

--- Program.cs
+++ Program.cs
@@ -3,11 +3,204 @@ using System.Collections.Generic;
 
 namespace BlackJackCS
 {
+    class Dealer
+    {
+        public string Card { get; set; }
+        public int Points()
+        {
+            var cardValue = 0;
+            switch (Card)
+            {
+                case "Ace of Clubs":
+                case "Ace of Hearts":
+                case "Ace of Spades":
+                case "Ace of Diamonds":
+                    cardValue = 1;
+                    break;
+                case "2 of Clubs":
+                case "2 of Hearts":
+                case "2 of Spades":
+                case "2 of Diamonds":
+                    cardValue = 2;
+                    break;
+                case "3 of Clubs":
+                case "3 of Hearts":
+                case "3 of Spades":
+                case "3 of Diamonds":
+                    cardValue = 3;
+                    break;
+                case "4 of Clubs":
+                case "4 of Hearts":
+                case "4 of Spades":
+                case "4 of Diamonds":
+                    cardValue = 4;
+                    break;
+                case "5 of Clubs":
+                case "5 of Hearts":
+                case "5 of Spades":
+                case "5 of Diamonds":
+                    cardValue = 5;
+                    break;
+                case "6 of Clubs":
+                case "6 of Hearts":
+                case "6 of Spades":
+                case "6 of Diamonds":
+                    cardValue = 6;
+                    break;
+                case "7 of Clubs":
+                case "7 of Hearts":
+                case "7 of Spades":
+                case "7 of Diamonds":
+                    cardValue = 7;
+                    break;
+                case "8 of Clubs":
+                case "8 of Hearts":
+                case "8 of Spades":
+                case "8 of Diamonds":
+                    cardValue = 8;
+                    break;
+                case "9 of Clubs":
+                case "9 of Hearts":
+                case "9 of Spades":
+                case "9 of Diamonds":
+                    cardValue = 9;
+                    break;
+                case "10 of Clubs":
+                case "10 of Hearts":
+                case "10 of Spades":
+                case "10 of Diamonds":
+                case "Jack of Clubs":
+                case "Jack of Hearts":
+                case "Jack of Spades":
+                case "Jack of Diamonds":
+                case "Queen of Clubs":
+                case "Queen of Hearts":
+                case "Queen of Spades":
+                case "Queen of Diamonds":
+                case "King of Clubs":
+                case "King of Hearts":
+                case "King of Spades":
+                case "King of Diamonds":
+                    cardValue = 10;
+                    break;
+                default:
+                    cardValue = 0;
+                    break;
+            }
+
+            return cardValue;
+        }
+        public void PrintCards()
+        {
+            Console.WriteLine($"Dealer Cards: {Card}");
+
+        }
+    }
+    class Player
+    {
+        public string Card { get; set; }
+        public int Points()
+        {
+            var cardValue = 0;
+            switch (Card)
+            {
+                case "Ace of Clubs":
+                case "Ace of Hearts":
+                case "Ace of Spades":
+                case "Ace of Diamonds":
+                    cardValue = 1;
+                    break;
+                case "2 of Clubs":
+                case "2 of Hearts":
+                case "2 of Spades":
+                case "2 of Diamonds":
+                    cardValue = 2;
+                    break;
+                case "3 of Clubs":
+                case "3 of Hearts":
+                case "3 of Spades":
+                case "3 of Diamonds":
+                    cardValue = 3;
+                    break;
+                case "4 of Clubs":
+                case "4 of Hearts":
+                case "4 of Spades":
+                case "4 of Diamonds":
+                    cardValue = 4;
+                    break;
+                case "5 of Clubs":
+                case "5 of Hearts":
+                case "5 of Spades":
+                case "5 of Diamonds":
+                    cardValue = 5;
+                    break;
+                case "6 of Clubs":
+                case "6 of Hearts":
+                case "6 of Spades":
+                case "6 of Diamonds":
+                    cardValue = 6;
+                    break;
+                case "7 of Clubs":
+                case "7 of Hearts":
+                case "7 of Spades":
+                case "7 of Diamonds":
+                    cardValue = 7;
+                    break;
+                case "8 of Clubs":
+                case "8 of Hearts":
+                case "8 of Spades":
+                case "8 of Diamonds":
+                    cardValue = 8;
+                    break;
+                case "9 of Clubs":
+                case "9 of Hearts":
+                case "9 of Spades":
+                case "9 of Diamonds":
+                    cardValue = 9;
+                    break;
+                case "10 of Clubs":
+                case "10 of Hearts":
+                case "10 of Spades":
+                case "10 of Diamonds":
+                case "Jack of Clubs":
+                case "Jack of Hearts":
+                case "Jack of Spades":
+                case "Jack of Diamonds":
+                case "Queen of Clubs":
+                case "Queen of Hearts":
+                case "Queen of Spades":
+                case "Queen of Diamonds":
+                case "King of Clubs":
+                case "King of Hearts":
+                case "King of Spades":
+                case "King of Diamonds":
+                    cardValue = 10;
+                    break;
+                default:
+                    cardValue = 0;
+                    break;
+            }
+
+            return cardValue;
+        }
+        public void PrintCards()
+        {
+            Console.WriteLine($"Player Cards: {Card}");
+
+        }
+    }
     class Program
     {
+
+        static string PromptForResponse(string prompt)
+        {
+            Console.Write(prompt);
+            var userInput = Console.ReadLine();
+            return userInput;
+        }
         static void Main(string[] args)
         {
-            // - Create a List of cards
+            // Create a List of cards
 
             var cardSuits = new List<string>() { "Clubs", "Diamonds", "Hearts", "Spades" };
             var cardValues = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
@@ -18,13 +211,11 @@ namespace BlackJackCS
                 foreach (var value in cardValues)
                 {
                     var newString = $"{value} of {suit}";
-                    Console.WriteLine($"{newString}");
-
                     cards.Add(newString);
                 }
             }
 
-            // - Shuffle Cards
+            // Shuffle Cards
 
             var numberOfCards = cards.Count;
 
@@ -37,9 +228,97 @@ namespace BlackJackCS
                 cards[leftIndex] = rightCard;
             }
 
-            var cardDealtOne = cards[0];
-            var cardDealtTwo = cards[1];
-            Console.WriteLine($"Cards dealt: {cardDealtOne} and {cardDealtTwo}");
+            // Create list of Dealer cards 
+            // - Pull 2 cards off top of deck (0,1) for Dealer. 
+            // (Need to save but not show yet)
+
+            var DealerCardOne = new Dealer
+            {
+                Card = cards[0],
+            };
+
+            var DealerCardTwo = new Dealer
+            {
+                Card = cards[1],
+            };
+
+            // - Create List Player cards - Pull 2 cards for the Player. (Print cards to screen)
+
+            var PlayerCardOne = new Player
+            {
+                Card = cards[2],
+            };
+
+            var PlayerCardTwo = new Player
+            {
+                Card = cards[3],
+            };
+
+            // - Display Player Cards
+
+            PlayerCardOne.PrintCards();
+            PlayerCardTwo.PrintCards();
+            Console.WriteLine(PlayerCardOne.Points() + PlayerCardTwo.Points());
+
+
+
+            // - If Player HITS:
+            //   -- Loop to add to Player card List - Pull 1 card for Player
+
+            var userInput = PromptForResponse("Would you like to HIT, or STAND? ");
+
+            if (userInput == "HIT" || userInput == "Hit" || userInput == "hit")
+            {
+                var PlayerCardThree = new Player
+                {
+                    Card = cards[4],
+                };
+                PlayerCardOne.PrintCards();
+                PlayerCardTwo.PrintCards();
+                PlayerCardThree.PrintCards();
+                Console.WriteLine(PlayerCardOne.Points() + PlayerCardTwo.Points() + PlayerCardThree.Points());
+
+            }
+            else if (userInput == "STAND" || userInput == "Stand" || userInput == "stand")
+            {
+                Console.WriteLine($"User selected STAND");
+                // Console.WriteLine($"Dealer Cards: {dealerCards[0]} and {dealerCards[1]} ");
+            }
+            else
+            {
+                Console.WriteLine($"Please make a valid selection");
+            }
+
+            // - Display Dealer Cards
+
+            DealerCardOne.PrintCards();
+            DealerCardTwo.PrintCards();
+
+            // - Prompt - Give option for Player to HIT or STAND
+
+
+
+            // - Check dealer cards total
+            //   -- If <= 16, pull another card until >= 17
+            //   -- If >= 17, no change
+            //   -- If Dealer goes over 21, BUST
+
+            if (DealerCardOne.Points() + DealerCardTwo.Points() <= 16)
+            {
+                Console.WriteLine(DealerCardOne.Points() + DealerCardTwo.Points());
+                Console.WriteLine($"Pick a card");
+            }
+            if (DealerCardOne.Points() + DealerCardTwo.Points() >= 17 && DealerCardOne.Points() + DealerCardTwo.Points() <= 21)
+            {
+                Console.WriteLine(DealerCardOne.Points() + DealerCardTwo.Points());
+                Console.Write($"Don't pick a card");
+            }
+            else if (DealerCardOne.Points() + DealerCardTwo.Points() >= 17 && DealerCardOne.Points() + DealerCardTwo.Points() < 21)
+            {
+                Console.WriteLine(DealerCardOne.Points() + DealerCardTwo.Points());
+                Console.Write($"Dealer is BUST!!");
+            }
         }
     }
+
 }
---
+++ SaveForLater.cs
@@ -0,0 +1,112 @@
+// foreach (var dealerCard in dealerCards)
+//             {
+//                 var cardValue = 0;
+//                 var listOfScores = new List<int>();
+//                 switch (dealerCard)
+//                 {
+//                     case "Ace of Clubs":
+//                     case "Ace of Hearts":
+//                     case "Ace of Spades":
+//                     case "Ace of Diamonds":
+//                         cardValue = 1;
+//                         listOfScores.Add(cardValue);
+//                         Console.WriteLine(cardValue);
+//                         break;
+//                     case "2 of Clubs":
+//                     case "2 of Hearts":
+//                     case "2 of Spades":
+//                     case "2 of Diamonds":
+//                         cardValue = 2;
+//                         listOfScores.Add(cardValue);
+//                         Console.WriteLine(cardValue);
+//                         break;
+//                     case "3 of Clubs":
+//                     case "3 of Hearts":
+//                     case "3 of Spades":
+//                     case "3 of Diamonds":
+//                         cardValue = 3;
+//                         listOfScores.Add(cardValue);
+//                         Console.WriteLine(cardValue);
+//                         break;
+//                     case "4 of Clubs":
+//                     case "4 of Hearts":
+//                     case "4 of Spades":
+//                     case "4 of Diamonds":
+//                         cardValue = 4;
+//                         listOfScores.Add(cardValue);
+//                         Console.WriteLine(cardValue);
+//                         break;
+//                     case "5 of Clubs":
+//                     case "5 of Hearts":
+//                     case "5 of Spades":
+//                     case "5 of Diamonds":
+//                         cardValue = 5;
+//                         listOfScores.Add(cardValue);
+//                         Console.WriteLine(cardValue);
+//                         break;
+//                     case "6 of Clubs":
+//                     case "6 of Hearts":
+//                     case "6 of Spades":
+//                     case "6 of Diamonds":
+//                         cardValue = 6;
+//                         listOfScores.Add(cardValue);
+//                         Console.WriteLine(cardValue);
+//                         break;
+//                     case "7 of Clubs":
+//                     case "7 of Hearts":
+//                     case "7 of Spades":
+//                     case "7 of Diamonds":
+//                         cardValue = 7;
+//                         listOfScores.Add(cardValue);
+//                         Console.WriteLine(cardValue);
+//                         break;
+//                     case "8 of Clubs":
+//                     case "8 of Hearts":
+//                     case "8 of Spades":
+//                     case "8 of Diamonds":
+//                         cardValue = 8;
+//                         listOfScores.Add(cardValue);
+//                         Console.WriteLine(cardValue);
+//                         break;
+//                     case "9 of Clubs":
+//                     case "9 of Hearts":
+//                     case "9 of Spades":
+//                     case "9 of Diamonds":
+//                         cardValue = 9;
+//                         listOfScores.Add(cardValue);
+//                         Console.WriteLine(cardValue);
+//                         break;
+//                     case "10 of Clubs":
+//                     case "10 of Hearts":
+//                     case "10 of Spades":
+//                     case "10 of Diamonds":
+//                     case "Jack of Clubs":
+//                     case "Jack of Hearts":
+//                     case "Jack of Spades":
+//                     case "Jack of Diamonds":
+//                     case "Queen of Clubs":
+//                     case "Queen of Hearts":
+//                     case "Queen of Spades":
+//                     case "Queen of Diamonds":
+//                     case "King of Clubs":
+//                     case "King of Hearts":
+//                     case "King of Spades":
+//                     case "King of Diamonds":
+//                         cardValue = 10;
+//                         listOfScores.Add(cardValue);
+//                         Console.WriteLine(cardValue);
+//                         break;
+//                     default:
+//                         cardValue = 0;
+//                         break;
+//                 }
+//                 // var dealerPoints = listOfScores[0] + listOfScores[1];
+//                 Console.WriteLine($"-----");
+//                 var score1 = listOfScores[0];
+//                 Console.WriteLine($"Score1: {score1}");
+//                 // var score2 = listOfScores[1];
+//                 // Console.WriteLine($"Score2: {score2}");
+//                 // var add = score1 + score2;
+
+//                 // Console.WriteLine($" Addition: {add}");
+//             }
